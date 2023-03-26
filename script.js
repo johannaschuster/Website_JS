cd51bf57b1936bc9ff179e5ecb08511f9f26e800
ON22LS <118619174+ON22LS@users.noreply.github.com>
2023-03-26 17:29:33 +0200

    delete

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const beispiel1 = document.querySelector("#beispiel1");
-beispiel1.addEventListener("click", executeExample);
 
-function executeExample() {
-  alert("das ist ein Beispiel für den Funtionsaufruf");
-}
